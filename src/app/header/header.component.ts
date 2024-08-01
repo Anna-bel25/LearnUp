ddc313c230ab296548a8319ae6b53ee430f7a5ad
María José Zhingri <mariazhingripaz@outlook.com>
2024-08-01 15:57:57 -0500

    cambios en componntes de login, register y cuenta de usuario

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,21 @@
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { ApiService } from '../api.service';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterLink } from '@angular/router';
-// import { ApiService } from '../api.service';
-// import { Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterLink, CommonModule ],
+  imports: [RouterLink,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  // userInfo: { tipocuenta: string, username: string } | null = null;
-  // private destroy$ = new Subject<void>();
-  // showOptions = false;
-
+  userInfo: { tipocuenta: string, username: string } | null = null;
+  private destroy$ = new Subject<void>();
+  showOptions = false;
 
   activeLinkIndex = 0;
   links = [
@@ -28,10 +28,11 @@ export class HeaderComponent {
     { path: '/login', label: 'Usuario', active: false },
     { path: '/cuenta-usuario',label:'Cuenta',active:false},
     { path: '/cuenta-usuario',label:'Cuenta',active:false},
+
+    // { path: '/menu-recurso', label: 'Contactanos', active: false },
   ];
 
-
-  constructor(private router: Router) { }
+  constructor(private router: Router,private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.router.events.subscribe(event => {
@@ -40,23 +41,15 @@ export class HeaderComponent {
       }
     });
 
-    // this.apiService.getUserInfo().subscribe(userInfo => {
-    //   this.userInfo = userInfo;
-    // });
+    this.apiService.getUserInfo().subscribe(userInfo => {
+      this.userInfo = userInfo;
+    });
 
-    // this.getUserInfo();
+    this.getUserInfo();
   }
-  // ngOnDestroy(): void {
-  //   this.destroy$.next();
-  //   this.destroy$.complete();
-  // }
-  // getUserInfo() {
-  //   this.userInfo = this.apiService.getUserInfoFromToken();
-  //   console.log('UserInfo:', this.userInfo); // Verificar en la consola
-  //   //this.cd.detectChanges();
-  // }
-  navigateToLogin() {
-    this.router.navigate(['/login']);
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   setActiveLink(index: number) {
@@ -68,5 +61,37 @@ export class HeaderComponent {
     return this.activeLinkIndex === index;
   }
 
+  // ir a la pagina de login --- Maria
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+  getUserInfo() {
+    this.userInfo = this.apiService.getUserInfoFromToken();
+    console.log('UserInfo:', this.userInfo); // Verificar en la consola
+    //this.cd.detectChanges();
+  }
+  logout() {
+    this.userInfo = null;
+    this.apiService.logout();
+    this.router.navigate(['/home']); // Navega a la página de inicio o login después de cerrar sesión
+    this.showOptions = false;
+  }
+  toggleOptions() {
+    this.showOptions = !this.showOptions;
+  }
 
-}
+  anotherOption() {
+    // Lógica para otra opción
+    console.log('Otra opción');
+    this.showOptions = false;
+  }
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    // Cierra las opciones si se hace clic fuera del botón y las opciones
+    const target = event.target as HTMLElement;
+    if (!target.closest('.fa-sign-out-alt') && !target.closest('.options-container')) {
+      this.showOptions = false;
+    }
+  }
+}  
+//--------------------------------
